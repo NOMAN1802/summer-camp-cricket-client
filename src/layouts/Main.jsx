b51802ca3f81676d98fd40161f055d8c298370fb
NOMAN1802 <mustakimnoman82@gmail.com>
2023-06-06 23:13:01 +0600

    initial route setup

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,10 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import Header from '../components/Shared/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/Shared/Footer';
 
 const Main = () => {
     return (
         <div>
-            
+            <Header></Header>
+            <div className='my-20'>
+            <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
