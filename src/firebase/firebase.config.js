94cd67fe6a1af1adba0f688f36065e63dbb20110
NOMAN1802 <mustakimnoman82@gmail.com>
2023-06-07 02:57:59 +0600

    react healmet & login page

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 
-const firebaseConfig = {
+ export const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.VITE_projectId,
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes.jsx'
-// import AuthProvider from './Pages/providers/AuthProvider'
+import AuthProvider from './Pages/providers/AuthProvider'
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     {/* <AuthProvider> */}
+     <AuthProvider>
+     <HelmetProvider>
      <RouterProvider router={router} />
-     {/* </AuthProvider> */}
+     </HelmetProvider>
+     </AuthProvider>
   </React.StrictMode>,
 )
